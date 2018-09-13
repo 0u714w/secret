c9ad81ccb5ce9f0d791bb360f3160b0e3aa96061
Doug Enas <douglassenas@gmail.com>
2018-09-13 15:43:19 -0400

    image folder and styles

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
-const monster1 = new Creature({
-    name: 'monster1',
-    health: 2000,
-})
-
-const monster2 = new Creature({
-    name: 'monster2',
-    health: 1000,
-    chanceToCrit: 0.1,
-    chanceToMiss: 0.1,
-    baseDamage: 1200,
-})
-
-const monster3 = new Creature({
-    name: 'monster3',
-    chanceToCrit: 0.05,
-})
-
-const monster4 = new Creature({
-    name: 'monster4',
+const mario = new Creature({
+    name: 'Mario',
     health: 100,
 })
 
-const monster5 = new Creature({
-    name: 'monster5',
+const luigi = new Creature({
+    name: 'Luigi',
+    health: 100,
+
 })
 
+const script = document.getElementById("main")
+const newWinner = document.getElementById("winner")
+
+// mario.prototype.attack = "Red Fireball"
+// luigi.prototype.attack = "Green Fireball"
+// peach.prototype.attack = "Turnip Throw"
+// toad.prototype.attack = "Mushroom Blast"
+// koopa.prototype.attack = "Shell Smash"
+
 function Creature(options) {
-    if (!options.name) {
-        throw (new Error("GIVE THE THING A NAME GOSHDANGIT."))
-    } else {
-        this.name = options.name
-    }
+    this.name = options.name
     this.health = options.health || 50
     this.maxHealth = this.health * 2
     this.chanceToCrit = options.chanceToCrit || 0.1
@@ -46,18 +35,20 @@ function Creature(options) {
                 this.baseDamage * 2 :
                 this.baseDamage
             creature.health -= dmg
-            message = `${creature.name} has been hit! It's now at ${creature.health} health.`
+            message = creature.name + " has been hit! It's now at " + creature.health + " health."
         }
-        console.log(message)
+        // console.log(message)
+        let newText = document.createTextNode(message)
+
+        script.appendChild(newText)
+        script.appendChild(document.createElement("br"));
+
+
         return message
     }
 }
 
-
 function battle(hero, ...monsters) {
-    if (monsters.length === 0) {
-        monsters = [new Creature({ name: "Martha Stewart Angry and Methed-Up" })]
-    }
 
     monsters.forEach(monster => {
         while (hero.health > 0 && monster.health > 0) {
@@ -69,10 +60,20 @@ function battle(hero, ...monsters) {
         hero.health = Math.random() > 0.9 ?
             hero.maxHealth :
             hero.health * 2
-        console.log(`${hero.name} is at ${hero.health} health and ${monster.name} is ${monster.health} health.`)
-        console.log(`${hero.health > 0 ? hero.name : monster.name} was the victor!`)
+        let newHealth = document.createTextNode(`${hero.name} is at ${hero.health} health and ${monster.name} is ${monster.health} health.`)
+
+        setTimeout(function() { alert(`${hero.health > 0 ? hero.name : monster.name} wins!`) }, 1000);
+        script.appendChild(newHealth)
+        script.appendChild(document.createElement("br"))
+        newWinner.appendChild(winner)
     })
 
 }
 
-battle(monster1, monster2, monster3, monster4)
+
+
+let button = document.getElementById("button")
+
+document.getElementById("reset").addEventListener("click", function() {
+    window.location.reload()
+})
